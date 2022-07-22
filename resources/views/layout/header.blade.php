0cd2abf20e15e764f45d3b76cef02657efba33d7
Your Name <onwabrains33@gmail.com>
2022-07-22 11:03:42 +0100

    optimization

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,12 +8,12 @@
     <link rel="shortcut icon" type="image/x-icon" href="/assets/img/logo/icon.ico">
 
 	<!-- CSS here -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/slicknav.min.css">
     <link rel="stylesheet" href="assets/css/flaticon.css">
     <link rel="stylesheet" href="assets/css/animate.min.css">
     <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
-    <link rel="stylesheet" href="assets/css/fontawesome-all.min.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
     <link rel="stylesheet" href="assets/css/slick.min.css">
     <link rel="stylesheet" href="assets/css/nice-select.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
